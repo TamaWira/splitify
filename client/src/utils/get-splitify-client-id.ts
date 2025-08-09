52649c76b1b57c20c9e6686212936a5c7b0b54c6
Bayu Adhya <asusrog20012001@gmail.com>
2025-08-09 22:35:18 +0700

    fix/client: fix getSplitifyClientId cookie splitting

--- client/src/utils/get-splitify-client-id.ts
+++ client/src/utils/get-splitify-client-id.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 
 export async function getSplitifyClientId() {
   const cookieStore = await cookies();
-  const clientId = cookieStore.get("splitify_client_id")?.value.split(" ")[1];
+  const clientId = cookieStore.get("splitify_client_id")?.value;
 
   return clientId;
 }
